74c435c76dda42584f669552fbb876a45cc499b6
USergei <serega.umerenkov@gmail.com>
2022-04-02 22:19:05 +0600

    using dotenv package for storing db connection credentionals Co-authored-by: Sergei Umerenkov <serega.umerenkov@gmail.com> Co-authored-by: Dmitrii Bondarenko <fructusmortus@gmail.com>

--- module-1/init-db.js
+++ module-1/init-db.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql')
+require('dotenv').config()
+const {DB_HOST, DB_USER, DB_PWD} = process.env
+
 const connection = mysql.createConnection({
-  host: 'db', 
-  user: 'root',
-  password: '123123'
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PWD
 })
+
 const queryStr = [`CREATE USER IF NOT EXISTS 'fashion'@'%' IDENTIFIED BY '123123';`,
   `CREATE DATABASE IF NOT EXISTS fashion;`,
   `GRANT ALL PRIVILEGES ON fashion.* to 'fashion'@'%' IDENTIFIED BY '123123';`,
@@ -16,6 +20,8 @@ const queryStr = [`CREATE USER IF NOT EXISTS 'fashion'@'%' IDENTIFIED BY '123123
     primary key (id)
 );`
 ]
+console.log({DB_HOST, DB_USER, DB_PWD})
+connection.connect()
 
 queryStr.forEach((element) => {
   connection.query(element, function (err) {
@@ -24,4 +30,40 @@ queryStr.forEach((element) => {
       return 
     }
   })
-})
+}) 
+
+connection.end()
+
+// connection.connect()
+
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+
+// connection.end();
+// queryStr.forEach((element) => {
+//   connection.query(element)
+// })         
+// .then(rows => {
+//   console.log(rows)
+//   // connection.end()
+// })
+// .catch(err => { 
+//   throw new Error(err)  
+// })
+
+// mariadb.createConnection({host: 'mydb.com', user: 'myUser', password: 'myPwd'})
+// .then(conn => {
+//   conn.query("select 1", [2])
+//     .then(rows => {
+//       console.log(rows); // [{ "1": 1 }]
+//       conn.end();
+//     })
+//     .catch(err => { 
+//       //handle query error
+//     });
+// })
+// .catch(err => {
+//   //handle connection error
+// });
