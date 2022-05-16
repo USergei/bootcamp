0af8ef7f5bf307bec50a65db5b17250d3ec9e874
Dmitrii Bondarenko <fructusmortus@gmail.com>
2022-05-16 22:19:16 +0600

    Charts added
    Co-authored-by: Sergei Umerenkov <serega.umerenkov@gmail.com>
    Co-authored-by: Dmitrii Bondarenko <fructusmortus@gmail.com>

--- module-3/src/components/Body/Dashboards/Dashboards.jsx
+++ module-3/src/components/Body/Dashboards/Dashboards.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import style from './Dashboards.module.scss'
 import DashboardsHeader from "./DashboardsHeader/DashboardsHeader"
 import StatsWidgetsBar from "./StatsWidgetsBar"
+import Charts from "./Charts/Charts"
 
 const Dashboards = () => {
     return (
         <div className={style.dashboards}>
             <DashboardsHeader/>
             <StatsWidgetsBar/>
+            <Charts/>
         </div>
     )
 }
