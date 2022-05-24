89f957b8aa30cfc654ad9172566e7d309d8dee3c
USergei <serega.umerenkov@gmail.com>
2022-05-24 21:07:05 +0600

    adding content to the review block
    Co-authored-by: Sergei Umerenkov <serega.umerenkov@gmail.com>
    Co-authored-by: Dmitrii Bondarenko <fructusmortus@gmail.com>

---
+++ module-3/src/components/Body/Dashboards/ReviewsWidget/Review/Review.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import style from './Review.module.scss'
+import SVG from 'react-inlinesvg' 
+import fullStar from './../../../../../assets/icons/fullStar.svg' 
+import emptyStar from './../../../../../assets/icons/emptyStar.svg' 
+import halfStar from './../../../../../assets/icons/halfStar.svg' 
+
+const Review = ({id, rating, dateTime, text, author}) => {
+
+    const getRating = rating => {
+        let indexes = []
+        for (let i = 1; i < 6; i++) {
+            if (i >= rating) {
+                indexes.push(<SVG src={fullStar} alt="likes" />)
+            }
+            if (i < rating) {
+                indexes.push(<SVG src={emptyStar} alt="likes" />) 
+            }
+            if (i % rating) {
+                indexes.push(<SVG src={halfStar} alt="likes" />)
+            }
+        }
+
+        return indexes
+    }
+
+    return (
+        <div className={style.reviewItems}>
+        <div className={style.reviewStatus}>
+          <div className={style.reviewRaiting}>
+            {getRating(rating)}
+          </div>
+          <div className={style.reviewTime}>
+              {dateTime}
+          </div>
+        </div>
+        <div className={style.reviewText}>
+          {text}
+        </div>
+        <div className={style.reviewName}>
+          {author}    
+        </div>
+      </div>
+    )
+}
+
+export default Review
--- module-3/src/components/Body/Dashboards/ReviewsWidget/ReviewsWidget.jsx
+++ module-3/src/components/Body/Dashboards/ReviewsWidget/ReviewsWidget.jsx
@@ -1,9 +1,49 @@
 import React from "react";
 import style from './ReviewsWidget.module.scss'
+import Review from "./Review"
+
+const reviewsWidgetContent = [
+  {
+    id: 1,
+    rating: 4.5,
+    dateTime: '1h 13min ago',
+    text: 'Lorem ipsum dolor sit amet consectetur',
+    author: '- Sujit Day '
+  },
+  {
+    id: 2,
+    rating: 3,
+    dateTime: '5dec 2022',
+    text: 'Lorem ipsum dolor sit amet consectetur',
+    author: '- Sujit Day '
+  },
+  {
+    id: 3,
+    rating: 1,
+    dateTime: '30min ago',
+    text: 'Lorem ipsum dolor sit amet consectetur',
+    author: '- Sujit Day '
+  },
+]
 
 const ReviewsWidget = () => {
   return (
-    <div className={style.reviewsWidget}><h1>Reviews</h1></div>
+    <div className={style.reviewsWidget}>
+      <div className={style.reviewsWidgetHeader}>
+        <h6>Reviews</h6>
+        <div>...</div>
+      </div>
+      {reviewsWidgetContent.map((item, i) =>
+        <Review
+          key={i}
+          id={item.id}
+          rating={item.rating}
+          dateTime={item.dateTime}
+          text={item.text}
+          author={item.author}
+        />
+      )}
+    </div>
   )
 }
 
