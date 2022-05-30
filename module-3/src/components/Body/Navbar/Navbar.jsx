7ffc839c8c1ba2648299a673474695d57a34281c
USergei <serega.umerenkov@gmail.com>
2022-05-30 20:31:20 +0600

    creating content for the navbar component
    Co-authored-by: Sergei Umerenkov <serega.umerenkov@gmail.com>
    Co-authored-by: Dmitrii Bondarenko <fructusmortus@gmail.com>

--- module-3/src/components/Body/Navbar/Navbar.jsx
+++ module-3/src/components/Body/Navbar/Navbar.jsx
@@ -1,30 +1,38 @@
-import React from "react"
+import React, {useState} from "react"
 import style from './Navbar.module.scss'
 import SVG from 'react-inlinesvg'
 import dashboard from './../../../assets/icons/dashboard.svg'
 import envelope from './../../../assets/icons/envelope.svg'
+import overview from './../../../assets/icons/overview.svg'
+import statistic from './../../../assets/icons/statistic.svg'
+import invoice from './../../../assets/icons/invoice.svg'
+import myAds from './../../../assets/icons/myAds.svg'
+import calendar from './../../../assets/icons/calendar.svg'
+import feedbackStar from './../../../assets/icons/feedbackStar.svg'
+import statement from './../../../assets/icons/statement.svg'
+import settings from './../../../assets/icons/settings.svg'
 
 const Navbar = () => {
     const sidebarNavigationMenu = {
         dashboard: {
             icon: dashboard,
             title: 'Dashboard',
-            compose: {
-                title: 'Compose',
-                url: '/messages/compose'
-            },
-            inbox: {
-                title: 'Inbox',
-                url: '/messages/inbox'
-            },
-            draft: {
-                title: 'Draft',
-                url: '/messages/draft'
-            },
-            sentbox: {
-                title: 'Sentbox',
-                url: '/messages/sentbox'
-            }
+            // compose: {
+            //     title: 'Compose',
+            //     url: '/messages/compose'
+            // },
+            // inbox: {
+            //     title: 'Inbox',
+            //     url: '/messages/inbox'
+            // },
+            // draft: {
+            //     title: 'Draft',
+            //     url: '/messages/draft'
+            // },
+            // sentbox: {
+            //     title: 'Sentbox',
+            //     url: '/messages/sentbox'
+            // }
         },
         messages: {
             icon: envelope,
@@ -47,21 +55,74 @@ const Navbar = () => {
                     url: '/messages/sentbox'
                 }
             }
+        },
+        overview: {
+            icon: overview,
+            title: 'Overview'
+        },
+        statistic: {
+            icon: statistic,
+            title: 'Statistic'
+        },
+        invoice: {
+            icon: invoice,
+            title: 'Invoice'
+        },
+        myAds: {
+            icon: myAds,
+            title: 'My Ads'
+        },
+        calendar: {
+            icon: calendar,
+            title: 'Calendar'
+        },
+        feedback: {
+            icon: feedbackStar,
+            title: 'Feedback'
+        },
+        statement: {
+            icon: statement,
+            title: 'Statement'
+        },
+        settings: {
+            icon: settings,
+            title: 'Settings'
         }
     }
 
+    const [sideBarMenuState, setSideBarMenuState] = useState({})
+    const menuItemOnClickHandler = menuItemKey => {
+        setSideBarMenuState({
+            ...sideBarMenuState,
+            [menuItemKey]: sideBarMenuState.menuItemKey? false : true
+        })
+    }
+
+    console.log(sideBarMenuState)
+    
     const buildMenu = menuObject => {
-        // console.log(Object.entries(menuObject))
         const menuItems = []
-        for (let [key,value] of Object.entries(menuObject)) {
-            // console.log(value);
+        for (let [topLevelMenuItemKey, topLevelMenu] of Object.entries(menuObject)) {
+            const subMenuItems = []
+            if (topLevelMenu.submenu ) {
+                for (let subMenuItem of Object.values(topLevelMenu.submenu)) {
+                    subMenuItems.push(
+                        <a className={style.subMenuItem} href={subMenuItem.url}>{subMenuItem.title}</a>
+                    )
+                }
+            }
             menuItems.push(
                 <nav>
-                    {<SVG className={style.navbarIcon} src={value.icon} alt="icon" />}
-                    <span>{value.title}</span>
-                    <nav>
-                        <a href=""></a>
-                    </nav>
+                    <div className={style.navbarFlex}>
+                        <SVG src={topLevelMenu.icon} alt="icon" />
+                        <span>{topLevelMenu.title}</span>
+                        <button onClick={() => menuItemOnClickHandler(topLevelMenuItemKey)}>+</button>
+                    </div>
+                    {subMenuItems.length > 0 && sideBarMenuState[topLevelMenuItemKey] &&
+                        <nav className={style.isClosed}>
+                            {subMenuItems}
+                        </nav>
+                    }       
                 </nav> 
             )
         }
@@ -70,7 +131,7 @@ const Navbar = () => {
     }
     return (
         <div className={style.navbar}>
-             {buildMenu(sidebarNavigationMenu)}
+            {buildMenu(sidebarNavigationMenu)}
            
         </div>
     )
