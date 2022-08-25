a0087b78fdedcd1a94a5615fed3c97a34fde077a
USergei <serega.umerenkov@gmail.com>
2022-08-25 22:15:06 +0600

    File getData was deleted. Axios installed and used in Projects component without success

--- module-3/web-app/src/components/Pages/Projects/Projects.jsx
+++ module-3/web-app/src/components/Pages/Projects/Projects.jsx
@@ -1,18 +1,20 @@
-import React from "react"
-import { getData } from "../../../services/getData"
+import React, {useState, useEffect} from "react"
+import  axios from 'axios'
 import style from "./Projects.module.scss"
 
-// const  data = async () => {
-//    return await getData('http://localhost:3001/selectAllProjects')
-// }
-// console.log('>>>>>>>', data())
-const data = getData('http://localhost:3001/selectAllProjects')
-console.log('>>>>>>>', data)
-
 const Projects = () => {
+    const [projects, setProjects] = useState('')
+   
+    useEffect(() => {
+        axios.get("http://localhost:3001/selectAllProjects")
+            .then(res => {
+                setProjects(res.data)
+            })
+    }, [])
+      
     return (
         <div> 
-            PROJECTS
+            {projects.title}
         </div>
     )
 }
