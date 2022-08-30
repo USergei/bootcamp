e28ff0e194cacd6c07e009cc134d5dd8f304860b
USergei <serega.umerenkov@gmail.com>
2022-08-30 22:15:06 +0600

    Axios was deleted and replaced with fetch in component Projects.

--- module-3/web-app/src/components/Pages/Projects/Projects.jsx
+++ module-3/web-app/src/components/Pages/Projects/Projects.jsx
@@ -1,21 +1,27 @@
 import React, {useState, useEffect} from "react"
-import  axios from 'axios'
 import style from "./Projects.module.scss"
 
+//TODO
 const Projects = () => {
     const [projects, setProjects] = useState('')
    
     useEffect(() => {
-        axios.get("http://localhost:3001/selectAllProjects")
-            .then(res => {
-                setProjects(res.data)
+        async function getData(url = "http://localhost:3001/selectAllProjects") {
+            const response = await fetch(url, {
+                method: 'GET'
             })
+
+            return response.json() 
+        }
+        getData().then(res => setProjects(res))
     }, [])
-      
+     
     return (
         <div> 
-            {projects.title}
-        </div>
+            {projects.length > 0 && projects.map((project, i) => (
+                <p key={i}>{project.title}</p> 
+            ))}
+        </div>   
     )
 }
 
