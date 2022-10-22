6c87bfc8aad6676ac1489e27ad2742c8c8530847
Dima <fructusmortus@gmail.com>
2022-10-22 07:54:02 +0600

    Useless import removed

--- module-3/web-app/src/store/store.js
+++ module-3/web-app/src/store/store.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, compose, legacy_createStore as createStore } from "redux"
-import { composeWithDevTools } from "redux-devtools-extension"
 import thunk from "redux-thunk"
 import reducer from "./reducers"
 
