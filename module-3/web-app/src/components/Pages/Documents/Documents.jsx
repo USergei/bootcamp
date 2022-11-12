158dcad2b6c5ec84a460ab6c2d8b3bc0262de53f
Dima <fructusmortus@gmail.com>
2022-11-12 23:24:44 +0600

    Some more styling added

--- module-3/web-app/src/components/Pages/Documents/Documents.jsx
+++ module-3/web-app/src/components/Pages/Documents/Documents.jsx
@@ -3,6 +3,21 @@ import {useSearchParams, Link} from "react-router-dom"
 import style from "./Documents.module.scss"
 import mainStyles from "../../../App.module.scss"
 
+const DOCUMENT_STATUSES = {
+    DRAFT: {
+        title: "draft",
+        color: "#7f4fae"
+    },
+    IN_REVIEW: {
+        title: "in review",
+        color: "#b74e1b"
+    },
+    PUBLISHED: {
+        title: "published",
+        color: "#2b680f"
+    }
+}
+
 //TODO move this in component fetch to a redux action later on
 const Documents = () => {
 
@@ -21,11 +36,16 @@ const Documents = () => {
             {documents.length > 0 && documents.map((document, i) => (
                 <Link className={style.document} key={i} to={`/document/${document.documentId}`}>
                     <div className={style.fileIcon}>
-                        <span/>
-                        <span/>
-                        <span/>
-                        <span/>
-                        <span>.doc</span> 
+                        <span className={style.line}/>
+                        <span className={style.line}/>
+                        <span className={style.line}/>
+                        <span className={style.line}/>
+                        <span 
+                            className={style.badge} 
+                            style={{backgroundColor: DOCUMENT_STATUSES["PUBLISHED"].color}}
+                        >
+                            {DOCUMENT_STATUSES["PUBLISHED"].title}
+                        </span> 
                         
                         {/* <div>Created: {document.createdAt}</div> 
                         <div>Updated: {document.updatedAt}</div>  */}
